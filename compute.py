4af93e2fea09e48d05eb2587478f36a8a51a5254
Robert Hitt <rhitt@gmu.edu>
2017-09-08 01:04:10 -0400

    Changed out_stack to a queue

--- compute.py
+++ compute.py
@@ -2,7 +2,7 @@
 Module for computing infix expressions and equations
 """
 
-from collections import namedtuple
+from collections import namedtuple, deque
 from io import BytesIO
 from tokenize import tokenize, NUMBER, OP
 
@@ -43,19 +43,19 @@ class ExpressionComputer:
         Converts an infix expression to a postfix expression using the Shunting-Yard algorithm.
 
         Returns:
-            a stack containing the operands and operators in postfix form
+            a queue containing the operands and operators in postfix order
         """
 
-        out_stack = []
+        out_queue = deque()
         op_stack = []
         for tok_type, tok_string, *_ in list(self.tokens):
             if tok_type == NUMBER:
-                out_stack.append((tok_type, tok_string))
+                out_queue.append((tok_type, tok_string))
             elif tok_string == '(':
                 op_stack.append((tok_type, tok_string))
             elif tok_string == ')':
                 while op_stack and op_stack[-1][1] != '(':
-                    out_stack.append(op_stack.pop())
+                    out_queue.append(op_stack.pop())
                 op_stack.pop()
             elif tok_type == OP:
                 while op_stack:
@@ -65,15 +65,15 @@ class ExpressionComputer:
 
                     op_info, tok_info = self.ops[operator], self.ops[tok_string]
                     if op_info.assoc == 'L' and op_info.prec >= tok_info.prec:
-                        out_stack.append(op_stack.pop())
+                        out_queue.append(op_stack.pop())
                     else:
                         break
                 op_stack.append((tok_type, tok_string))
 
         while op_stack:
-            out_stack.append(op_stack.pop())
+            out_queue.append(op_stack.pop())
 
-        return out_stack
+        return out_queue
 
 
 com = ExpressionComputer('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3')
