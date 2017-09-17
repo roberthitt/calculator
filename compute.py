17995388d64625e98227ccd89a7600b0e26662e2
Robert Hitt <rhitt@gmu.edu>
2017-09-16 20:21:48 -0400

    Refactored calculations to use Numpy functions. - Speeds have been increased by literally 500x

--- compute.py
+++ compute.py
@@ -2,8 +2,6 @@
 Module for computing infix expressions and equations
 """
 
-import operator
-import math
 from collections import namedtuple, deque
 from io import BytesIO
 from tokenize import tokenize, NUMBER, ENCODING
@@ -13,6 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+NP_ARRAY = 999
 class Calculator:
     """
     Class for computing infix expressions.
@@ -26,21 +25,20 @@ class Calculator:
 
     def __init__(self):
         ## TODO: consider moving this to a seperate config file
-        self.OPS = {
-            'neg': self.OpInfo(precedence=5, assoc='R', operation=operator.neg, operand_count=1),
-            'abs': self.OpInfo(precedence=4, assoc='L', operation=operator.abs, operand_count=1),
-            'sin': self.OpInfo(precedence=4, assoc='L', operation=math.sin, operand_count=1),
-            'cos': self.OpInfo(precedence=4, assoc='L', operation=math.cos, operand_count=1),
-            'tan': self.OpInfo(precedence=4, assoc='L', operation=math.tan, operand_count=1),
-            'sqrt': self.OpInfo(precedence=4, assoc='L', operation=math.sqrt, operand_count=1),
-            'log': self.OpInfo(precedence=4, assoc='L', operation=math.log10, operand_count=1),
-            'ln': self.OpInfo(precedence=4, assoc='L', operation=math.log, operand_count=1),
-            '!': self.OpInfo(precedence=4, assoc='R', operation=math.factorial, operand_count=1),
-            '^': self.OpInfo(precedence=3, assoc='R', operation=operator.pow, operand_count=2),
-            '*': self.OpInfo(precedence=2, assoc='L', operation=operator.mul, operand_count=2),
-            '/': self.OpInfo(precedence=2, assoc='L', operation=operator.truediv, operand_count=2),
-            '+': self.OpInfo(precedence=1, assoc='L', operation=operator.add, operand_count=2),
-            '-': self.OpInfo(precedence=1, assoc='L', operation=operator.sub, operand_count=2)
+        self.ops = {
+            'neg': self.OpInfo(precedence=5, assoc='R', operation=np.negative, operand_count=1),
+            'abs': self.OpInfo(precedence=4, assoc='L', operation=np.absolute, operand_count=1),
+            'sin': self.OpInfo(precedence=4, assoc='L', operation=np.sin, operand_count=1),
+            'cos': self.OpInfo(precedence=4, assoc='L', operation=np.cos, operand_count=1),
+            'tan': self.OpInfo(precedence=4, assoc='L', operation=np.tan, operand_count=1),
+            'sqrt': self.OpInfo(precedence=4, assoc='L', operation=np.sqrt, operand_count=1),
+            'log': self.OpInfo(precedence=4, assoc='L', operation=np.log10, operand_count=1),
+            'ln': self.OpInfo(precedence=4, assoc='L', operation=np.log, operand_count=1),
+            '^': self.OpInfo(precedence=3, assoc='R', operation=np.power, operand_count=2),
+            '*': self.OpInfo(precedence=2, assoc='L', operation=np.multiply, operand_count=2),
+            '/': self.OpInfo(precedence=2, assoc='L', operation=np.true_divide, operand_count=2),
+            '+': self.OpInfo(precedence=1, assoc='L', operation=np.add, operand_count=2),
+            '-': self.OpInfo(precedence=1, assoc='L', operation=np.subtract, operand_count=2)
         }
 
     def graph(self, equation, dimensions=(10, 10)):
@@ -48,23 +46,23 @@ class Calculator:
         Graphs a function of the form 'y = x + 5'.
 
         Args:
-            equation: a string containing the right side of an equation (the left side is implied)
-            dimensions: a tuple of the form (x, y). domain will go from -x to x, and range from -y to y
+            equation: a string containing the right side of an equation (the left side is implied).
+            dimensions: a tuple of form (x, y). domain will go from -x to x, and range from -y to y.
         """
 
         # Note: if performance is an issue, consider rewriting to use np operators
         #       to operate on the entire np array rather than individual elements
 
-        x, y = dimensions
+        x_bounds, y_bounds = dimensions
 
-        increments = np.linspace(-x, x, 1000)
-        points = np.fromiter((self.solve(equation, replacement=value) for value in increments), np.float)
+        increments = np.linspace(-x_bounds, x_bounds, 1000)
+        points = self.solve(equation, replacement=increments)
 
-        # Necessary for functions with discontinuous lines.
-        points[points > y] = np.nan
-        points[points < -y] = np.nan
+        # Filters out too large/small values. Necessary for functions with discontinuous lines (e.g., tan).
+        points[points > y_bounds] = np.nan
+        points[points < -y_bounds] = np.nan
 
-        self.create_plot(increments, points, y)
+        self.create_plot(increments, points, y_bounds)
 
     def create_plot(self, scale, points, y_bounds):
         """
@@ -80,19 +78,20 @@ class Calculator:
         """
 
         fig = plt.figure(1)
-        ax = SubplotZero(fig, 111)
-        fig.add_subplot(ax)
+        subplot = SubplotZero(fig, 111)
+        fig.add_subplot(subplot)
 
         for direction in ['xzero', 'yzero']:
-            ax.axis[direction].set_axisline_style('-|>')
-            ax.axis[direction].set_visible(True)
+            subplot.axis[direction].set_axisline_style('-|>')
+            subplot.axis[direction].set_visible(True)
 
         for direction in ['left', 'right', 'bottom', 'top']:
-            ax.axis[direction].set_visible(False)
+            subplot.axis[direction].set_visible(False)
 
-        ax.set_ylim([-y_bounds, y_bounds])
-        ax.plot(scale, points)
+        subplot.set_ylim([-y_bounds, y_bounds])
+        subplot.plot(scale, points)
 
+        # TODO: return some sort of serializable version of graph
         plt.show()
 
     def solve(self, expression, replacement=None):
@@ -104,22 +103,25 @@ class Calculator:
 
         Args:
             expression: a string containing an algebraic expression in infix notation
-            replacement: optional value to replace the variable 'x' if it occurs in expression
+            replacement: optional Numpy array to replace the variable 'x' if it occurs in expression
 
         Returns:
-            integer or float result of the expression
+            float result of the expression
         """
 
         if replacement is not None:
-            expression = expression.replace('x', f'({replacement})')
+            # Necessary for supporting expressions like 5x.
+            expression = expression.replace('x', '(x)')
 
-        postfix_queue = self.convert_infix(expression)
+        postfix_queue = self.convert_infix(expression, replacement)
         eval_stack = []
 
         try:
             for token in postfix_queue:
-                if token in self.OPS:
-                    *_, operation, operand_count = self.OPS[token]
+                if isinstance(token, np.ndarray):
+                    eval_stack.append(token)
+                elif token in self.ops:
+                    *_, operation, operand_count = self.ops[token]
 
                     # Pops a variable number of items off the stack.
                     operands = eval_stack[-operand_count:]
@@ -136,7 +138,7 @@ class Calculator:
             return None
 
 
-    def convert_infix(self, expression):
+    def convert_infix(self, expression, replacement=None):
         """
         Converts an infix expression to a postfix expression using the Shunting-Yard algorithm.
 
@@ -145,6 +147,7 @@ class Calculator:
 
         Args:
             expression: a string containing an algebraic expression in infix notation
+            replacement: optional Numpy array to replace the variable 'x' if it occurs in expression
 
         Returns:
             a queue of tuples containing operands and operators in postfix order
@@ -152,6 +155,12 @@ class Calculator:
 
         # Breaks the expression string into a list of tokens, expressed as 5-tuples.
         tokens = list(tokenize(BytesIO(expression.encode('utf_8')).readline))
+
+        # Replaces X value with Numpy array.
+        if replacement is not None:
+            tokens = [(NP_ARRAY, replacement, token[2], token[2], token[3])
+                      if token[1] == 'x' else token
+                      for token in tokens]
 
         out_queue = deque()
         op_stack = []
@@ -162,10 +171,11 @@ class Calculator:
 
             if tok_type == NUMBER:
                 out_queue.append(float(tok_string))
-
+            elif tok_type == NP_ARRAY:
+                out_queue.append(tok_string)
             elif tok_string == '(':
-                # Allow implicit multiplication (eg 5(x) or (x)(y))
-                if prev_tok_type == NUMBER or prev_tok_string == ')':
+                # Allow implicit multiplication (e.g., c(x) or (x)(y)).
+                if prev_tok_type == NUMBER or prev_tok_type == NP_ARRAY or prev_tok_string == ')':
                     op_stack.append('*')
 
                 op_stack.append(tok_string)
@@ -175,19 +185,19 @@ class Calculator:
                     out_queue.append(op_stack.pop())
                 op_stack.pop()
 
-            elif tok_string in self.OPS:
-                # Allow unary minus operator
-                if tok_string == '-' and (prev_tok_string in self.OPS or prev_tok_string == '('
-                    or prev_tok_string == None) and prev_tok_string != ')':
+            elif tok_string in self.ops:
+                # Allow unary minus operator.
+                if (tok_string == '-' and prev_tok_string != ')' and
+                        (prev_tok_type == NP_ARRAY or prev_tok_string in self.ops
+                         or prev_tok_string == '(' or prev_tok_string is None)):
 
                     tok_string = 'neg'
-
                 while op_stack:
                     top = op_stack[-1]
                     if top == '(':
                         break
 
-                    top_info, current_info = self.OPS[top], self.OPS[tok_string]
+                    top_info, current_info = self.ops[top], self.ops[tok_string]
                     if top_info.assoc == 'L' and top_info.precedence >= current_info.precedence:
                         out_queue.append(op_stack.pop())
                     else:
@@ -203,6 +213,4 @@ class Calculator:
         return out_queue
 
 com = Calculator()
-com.graph('sqrt(x) + 5')
-com.graph('log(x) + 5')
-com.graph('ln(x) + 5')
+com.graph('tan(x)')
