36a3cb49195e5059462fb0bd8951b6dfdfffe628
Robert Hitt <rhitt@gmu.edu>
2017-11-05 01:24:48 -0400

    This should fix Heroku type errors.

--- service/server.py
+++ service/server.py
@@ -27,7 +27,7 @@ async def graph(request):
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=os.environ.get('PORT') or 8080,
+        port=int(os.environ.get('PORT')) or 8080,
         workers=4,
         debug=True
     )
