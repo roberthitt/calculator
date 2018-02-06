ad0a80c72791a9430e3606f6539bbfa978bb69bc
Robert Hitt <rhitt@gmu.edu>
2018-02-05 20:39:11 -0500

    Added basic graphing to front-end.

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Input, Container, Header, } from 'semantic-ui-react'
+import { Input, Container, Header, Image } from 'semantic-ui-react'
 
 class EquationInput extends React.Component {
+    render() {
+        const value = this.props.value;
+        return (
+            <Input placeholder='(enter an equation)'
+                   label='f(x) = ' fluid
+                   value={value}
+                   onChange={(event, data) => this.props.onChange(data.value)}/>
+        );
+    }
+}
+
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: '',
+            text: 'x',
         };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(value) {
@@ -15,21 +28,13 @@ class EquationInput extends React.Component {
     }
 
     render() {
-        return (
-            <Input placeholder='(enter an equation)'
-                   label='f(x) = ' fluid
-                   value={this.state.text}
-                   onChange={(event, data) => this.handleChange(data.value)}/>
-        );
-    }
-}
-
-class App extends React.Component {
-    render() {
+        const imageSource = 'http://localhost:8080/graph?exp=' + this.state.text;
         return (
             <Container style={{marginTop: '3em'}}>
                 <Header as='h1' dividing>Calculator</Header>
-                <EquationInput />
+                <Image bordered rounded centered
+                       src={imageSource} />
+                <EquationInput value={this.state.text} onChange={this.handleChange}/>
             </Container>
         );
     }
