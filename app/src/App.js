0a6c150e8898ff4092048ef9e002f3a5fa270382
Robert Hitt <rhitt@gmu.edu>
2018-02-10 23:51:48 -0500

    Graphing no longer done on each key press.

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Input, Container, Header, Image } from 'semantic-ui-react';
+import { Input, Container, Header, Image, Grid } from 'semantic-ui-react';
 
 const SERVICE_URL = 'http://localhost:8080';
 
@@ -9,9 +9,10 @@ class EquationInput extends React.Component {
         const value = this.props.value;
         return (
             <Input placeholder='(enter an equation)'
-                   label='f(x) = ' fluid
+                   label='f(x) = ' size='big'
                    value={value}
-                   onChange={(event, data) => this.props.onChange(data.value)}/>
+                   onChange={(event, data) => this.props.onChange(data.value)}
+                   onKeyPress={(e) => this.props.onKeyPress(e.key, value)}/>
         );
     }
 }
@@ -25,27 +26,40 @@ class App extends React.Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleChange(value) {
-        const validPath = encodeURI(SERVICE_URL + '/valid?exp=' + value);
-        axios.get(validPath).then(response => {
-            if(response.data.localeCompare('valid') === 0) {
-                this.setState({text: value, value: value});
-            } else {
-                this.setState({text: value});
-            }
-        });
+        this.setState({text: value});
+    }
+
+    handleKeyPress(key, value) {
+        if(key === 'Enter') {
+            const validPath = encodeURI(SERVICE_URL + '/valid?exp=' + value);
+            axios.get(validPath).then(response => {
+                if(response.data.localeCompare('valid') === 0) {
+                    this.setState({text: value, value: value});
+                }
+            });
+        }
     }
 
     render() {
-        const imageSource = encodeURI(SERVICE_URL + '/graph?exp=' + this.state.text);
+        const imageSource = encodeURI(SERVICE_URL + '/graph?exp=' + this.state.value);
         return (
-            <Container style={{marginTop: '3em'}}>
+            <Container style={{marginTop: '1em'}}>
                 <Header as='h1' dividing>Calculator</Header>
-                <Image bordered rounded centered
-                       src={imageSource} />
-                <EquationInput value={this.state.text} onChange={this.handleChange}/>
+                <Grid>
+                    <Grid.Row>
+                        <Image bordered rounded centered
+                            src={imageSource} />
+                    </Grid.Row>
+                    <Grid.Row centered>
+                        <EquationInput value={this.state.text}
+                                       onKeyPress={this.handleKeyPress}
+                                       onChange={this.handleChange}/>
+                    </Grid.Row>
+                </Grid>
             </Container>
         );
     }
